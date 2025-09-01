08c10c0276baed45a64dcfc8d141f9d1e95c917c
amlanbhuyan <amlankumarbhuyan007@gmail.com>
2025-09-01 15:28:43 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,12 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
+    }
+
+
+    emailjs.send("service_iueg226", "template_5lsaj3j", parms).then(alert ("Email Sent!!"));
+
+}
